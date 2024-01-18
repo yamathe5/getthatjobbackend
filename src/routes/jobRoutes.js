a566b263df247d8c772835e5413bf753458c63cb
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-01-18 12:42:03 -0500

    ADD: Jobs by company route

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -3,6 +3,7 @@ const jobController = require('../controllers/jobController');
 const router = express.Router();
 
 router.get('/jobs', jobController.getJobs);
+router.get('/companys/:companyId/jobs', jobController.getJobsByCompany);
 router.post('/jobs', jobController.createJob);
 router.put('/jobs/:id', jobController.updateJob);
 
