25d79cd2c571471aaaea04ae8753c01891717bcf
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-01-20 12:40:54 -0500

    ADD: following controller

---
+++ src/controllers/followingController.js
@@ -0,0 +1,46 @@
+// followingController.js
+const followingModel = require("../models/followingModel");
+
+const getFollowings = async (req, res) => {
+  const { professionalId } = req.params;
+  try {
+    const jobs = await followingModel.getFollowings(professionalId);
+    res.status(200).json(jobs);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const createFollow = async (req, res) => {
+  const {  professionalId } = req.params;
+  try {
+    const jobs = await followingModel.createFollow( professionalId, req.body);
+    res.status(200).json(jobs);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const deleteFollow = async (req, res) => {
+  const { followingId } = req.params;
+
+  // Verificar si followingId es un número válido o no
+  if (!followingId || isNaN(Number(followingId))) {
+    return res.status(400).send("Invalid ID.");
+  }
+
+  try {
+    const deletedCount = await followingModel.deleteFollow(followingId);
+    // console.log("req params", req.params, "deletedCount:", deletedCount);
+
+    if (deletedCount === 0) {
+      res.status(404).json({ message: `Following with ID ${followingId} not found.` });
+    } else {
+      res.status(200).json({ message: `Following with ID ${followingId} deleted successfully.` });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = { getFollowings, createFollow, deleteFollow };
