c167ae003bdb06c4ab70111d61493e8bd852e00b
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-01-23 19:29:01 -0500

    UPDATE: Routes in jobRoutes

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const jobController = require('../controllers/jobController');
 const router = express.Router();
 
+router.get('/professionals/:professionalId/jobs', jobController.getJobs);
+router.get('/professionals/:professionalId/jobs/:jobId', jobController.getJobById);
 router.get('/jobs', jobController.getJobs);
 router.get('/companys/:companyId/jobs', jobController.getJobsByCompany);
 router.post('/jobs', jobController.createJob);
