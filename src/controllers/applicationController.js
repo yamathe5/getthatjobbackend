ca928d40032a2b06c040e2d171482f54cc456db3
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-01-26 18:38:12 -0500

    ADD: get candidates by job function in applicationscontroller

--- src/controllers/applicationController.js
+++ src/controllers/applicationController.js
@@ -27,34 +27,79 @@ getApplicationsByCompany = async (req, res) => {
   // Lógica para obtener todas las aplicaciones para una compañía
 };
 
+getCandidatesByJob = async (req, res) => {
+  const { jobId } = req.params;
+  try {
+    const applications = await applicationModel.getCandidatesByJob(
+      jobId
+    );
+    res.status(200).json(applications);
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
 createApplication = async (req, res) => {
   // Lógica para crear una nueva aplicación
-  const {  professionalId } = req.params;
+  const { professionalId } = req.params;
   try {
-    const application = await applicationModel.createApplication( req.body);
+    const applicationExist = await applicationModel.checkIfExistApplication(
+      req.body
+    );
+
+    if (applicationExist) {
+      return res.status(409).json({ message: "Applicatio nalready exist." });
+    }
+    const application = await applicationModel.createApplication(req.body);
     res.status(200).json(application);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-updateApplication = async (req, res) => {
-  // Lógica para actualizar una aplicación
-};
+
 
 deleteApplication = async (req, res) => {
   const { professionalId, applicationId } = req.params;
   try {
-    const rowCount = await applicationModel.deleteApplication(professionalId, applicationId);
+    const rowCount = await applicationModel.deleteApplication(
+      professionalId,
+      applicationId
+    );
     if (rowCount > 0) {
-      res.status(200).json({ message: `Application ${applicationId} deleted successfully.` });
+      res
+        .status(200)
+        .json({
+          message: `Application ${applicationId} deleted successfully.`,
+        });
     } else {
-      res.status(404).json({ message: `Application ${applicationId} not found or you do not have permission to delete it.` });
+      res
+        .status(404)
+        .json({
+          message: `Application ${applicationId} not found or you do not have permission to delete it.`,
+        });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
+updateApplication = async (req, res) => {
+  const { applicationId } = req.params;
+  const { status } = req.body; // Asumimos que el cuerpo de la solicitud solo tiene el estado a actualizar
+
+  try {
+    const updatedApplication = await applicationModel.updateApplication(applicationId, status);
+    if (updatedApplication) {
+      return res.status(200).json(updatedApplication);
+    } else {
+      return res.status(404).json({ message: "Application not found." });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 
 module.exports = {
   getApplicationsByProfessional,
@@ -62,6 +107,7 @@ module.exports = {
   createApplication,
   updateApplication,
   deleteApplication,
+  getCandidatesByJob,
 };
 
 // Otros métodos de controlador...
